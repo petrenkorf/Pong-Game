78e3e8050d96a40fc9408817aa63632590aa0f1d
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2018-05-05 16:18:53 -0300

    First Commit

---
+++ src/pong_game/core.clj
@@ -0,0 +1,48 @@
+(ns pong-game.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(def left-racket  (atom {:x 10 :y 20 :w 15 :h 80}))
+(def right-racket (atom {:x 575 :y 20 :w 15 :h 80}))
+
+(defn setup []
+  (q/frame-rate 60))
+
+(defn update-state [state])
+
+(defn draw-racket [r]
+  (q/rect (:x r) (:y r) (:w r) (:h r)))
+
+(defn draw-state [state]
+  (q/background 0x20)
+  (q/fill 0xff)
+  (draw-racket @left-racket)
+  (draw-racket @right-racket))
+
+; Input
+
+(defn keypress [p1 key-data]
+  (cond
+    
+    ; left
+    (= (:key-code key-data) 87) 
+      (swap! left-racket update-in [:y] (fn [v] (- v 10))) 
+    (= (:key-code key-data) 83) 
+      (swap! left-racket update-in [:y] (fn [v] (+ v 10))) 
+    
+    ; right
+    (= (:key-code key-data) 38) 
+      (swap! right-racket update-in [:y] (fn [v] (- v 10))) 
+    (= (:key-code key-data) 40) 
+      (swap! right-racket update-in [:y] (fn [v] (+ v 10)))))
+
+(defn -main [& args]
+  (q/defsketch pong-game
+    :title "Pong Game"
+    :size [600 450]
+    :setup setup
+    :update update-state
+    :key-pressed keypress
+    :draw draw-state
+    :features [:keep-on-top]
+    :middleware [m/fun-mode]))
